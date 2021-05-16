e884ac204385e9e6894b0ab71697c88107bfa69a
91JackMa <53609318+91JackMa@users.noreply.github.com>
2021-05-16 16:41:09 +0800

    子仓库测试

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
     context = {}
-    return render(request, 'myhome.html', context)
+    return render(request, 'home.html', context)
